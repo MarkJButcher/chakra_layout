b463dcbb37a4c4f342118ddfaf1bab6a4b0d73b2
MarkJButcher <97991973+MarkJButcher@users.noreply.github.com>
2022-03-15 09:27:43 +1100

    .

--- src/components/sidebar/sidebar-data.js
+++ src/components/sidebar/sidebar-data.js
@@ -1,217 +1,217 @@
 /* eslint-disable import/no-anonymous-default-export */
-import { PhoneIcon, AddIcon, WarningIcon } from '@chakra-ui/icons'
+// import { PhoneIcon, AddIcon, WarningIcon } from '@chakra-ui/icons'
 
-// list of data to be displyaed in the sideboard and the routes for the paths
-// TODO: add required scope to these
-export default {
-  data: [
-    {
-      name: 'Dashboards',
-      icon: <Icons.DashboardRounded />,
-      id: 1,
-      children: [
-        {
-          name: 'TCAP Dashboard',
-          path: '/',
-          icon: <Icons.LoopRounded />,
-          id: 1.1,
-        },
-        {
-          name: 'Call Stats',
-          path: '/callstats',
-          icon: <Icons.ShowChartRounded />,
-          id: 1.2,
-        },
-      ],
-    },
-    {
-      name: 'Accounts',
-      icon: <Icons.AccountBoxRounded />,
-      id: 2,
-      children: [
-        {
-          name: 'Admins',
-          path: '/admins',
-          icon: <Icons.SupervisorAccountRounded />,
-          id: 2.1,
-        },
-        {
-          name: 'Permissions',
-          path: '/permissions',
-          icon: <Icons.VpnKeyRounded />,
-          id: 2.2,
-        },
-        {
-          name: 'Resource Accounts',
-          path: '/resourceaccounts',
-          icon: <Icons.SupervisorAccountRounded />,
-          id: 2.3,
-        },
+// // list of data to be displyaed in the sideboard and the routes for the paths
+// // TODO: add required scope to these
+// export default {
+//   data: [
+//     {
+//       name: 'Dashboards',
+//       icon: <Icons.DashboardRounded />,
+//       id: 1,
+//       children: [
+//         {
+//           name: 'TCAP Dashboard',
+//           path: '/',
+//           icon: <Icons.LoopRounded />,
+//           id: 1.1,
+//         },
+//         {
+//           name: 'Call Stats',
+//           path: '/callstats',
+//           icon: <Icons.ShowChartRounded />,
+//           id: 1.2,
+//         },
+//       ],
+//     },
+//     {
+//       name: 'Accounts',
+//       icon: <Icons.AccountBoxRounded />,
+//       id: 2,
+//       children: [
+//         {
+//           name: 'Admins',
+//           path: '/admins',
+//           icon: <Icons.SupervisorAccountRounded />,
+//           id: 2.1,
+//         },
+//         {
+//           name: 'Permissions',
+//           path: '/permissions',
+//           icon: <Icons.VpnKeyRounded />,
+//           id: 2.2,
+//         },
+//         {
+//           name: 'Resource Accounts',
+//           path: '/resourceaccounts',
+//           icon: <Icons.SupervisorAccountRounded />,
+//           id: 2.3,
+//         },
 
-        {
-          name: 'Users',
-          path: '/users',
-          icon: <Icons.AccountCircleRounded />,
-          id: 2.4,
-        },
-      ],
-    },
-    {
-      name: 'Automation',
-      icon: <Icons.DeviceHubRounded />,
-      id: 3,
-      children: [
-        {
-          name: 'API',
-          path: '/api',
-          icon: <Icons.CodeRounded />,
-          id: 3.1,
-        },
+//         {
+//           name: 'Users',
+//           path: '/users',
+//           icon: <Icons.AccountCircleRounded />,
+//           id: 2.4,
+//         },
+//       ],
+//     },
+//     {
+//       name: 'Automation',
+//       icon: <Icons.DeviceHubRounded />,
+//       id: 3,
+//       children: [
+//         {
+//           name: 'API',
+//           path: '/api',
+//           icon: <Icons.CodeRounded />,
+//           id: 3.1,
+//         },
 
-        {
-          name: 'Triggers',
-          path: '/triggers',
-          icon: <Icons.TungstenRounded />,
-          id: 3.2,
-        },
-        {
-          name: 'Jobs',
-          path: '/jobs',
-          icon: <Icons.HandymanRounded />,
-          id: 3.3,
-        },
-      ],
-    },
-    {
-      name: 'Billing',
-      icon: <Icons.MonetizationOnRounded />,
-      id: 4,
-      children: [
-        {
-          name: 'International Rates',
-          path: '/internationalrates',
-          icon: <Icons.PublicRounded />,
-          id: 4.1,
-        },
-        {
-          name: 'Invoices',
-          path: '/invoices',
-          icon: <Icons.ReceiptRounded />,
-          id: 4.2,
-        },
-        {
-          name: 'Plans',
-          path: '/plans',
-          icon: <Icons.FormatListNumberedRounded />,
-          id: 4.3,
-        },
-      ],
-    },
-    {
-      name: 'Organisations',
-      icon: <Icons.MediationRounded />,
-      id: 5,
-      children: [
-        {
-          name: 'Companies',
-          path: '/companies',
-          icon: <Icons.BusinessCenterRounded />,
-          id: 5.2,
-        },
-        {
-          name: 'Partners',
-          path: '/partners',
-          icon: <Icons.HomeWorkRounded />,
-          id: 5.3,
-        },
-      ],
-    },
-    {
-      name: 'Telephony',
-      icon: <Icons.PhoneRounded />,
-      id: 6,
-      children: [
-        {
-          name: 'Calls',
-          path: '/calls',
-          icon: <Icons.PhoneForwardedRounded />,
-          id: 6.1,
-        },
+//         {
+//           name: 'Triggers',
+//           path: '/triggers',
+//           icon: <Icons.TungstenRounded />,
+//           id: 3.2,
+//         },
+//         {
+//           name: 'Jobs',
+//           path: '/jobs',
+//           icon: <Icons.HandymanRounded />,
+//           id: 3.3,
+//         },
+//       ],
+//     },
+//     {
+//       name: 'Billing',
+//       icon: <Icons.MonetizationOnRounded />,
+//       id: 4,
+//       children: [
+//         {
+//           name: 'International Rates',
+//           path: '/internationalrates',
+//           icon: <Icons.PublicRounded />,
+//           id: 4.1,
+//         },
+//         {
+//           name: 'Invoices',
+//           path: '/invoices',
+//           icon: <Icons.ReceiptRounded />,
+//           id: 4.2,
+//         },
+//         {
+//           name: 'Plans',
+//           path: '/plans',
+//           icon: <Icons.FormatListNumberedRounded />,
+//           id: 4.3,
+//         },
+//       ],
+//     },
+//     {
+//       name: 'Organisations',
+//       icon: <Icons.MediationRounded />,
+//       id: 5,
+//       children: [
+//         {
+//           name: 'Companies',
+//           path: '/companies',
+//           icon: <Icons.BusinessCenterRounded />,
+//           id: 5.2,
+//         },
+//         {
+//           name: 'Partners',
+//           path: '/partners',
+//           icon: <Icons.HomeWorkRounded />,
+//           id: 5.3,
+//         },
+//       ],
+//     },
+//     {
+//       name: 'Telephony',
+//       icon: <Icons.PhoneRounded />,
+//       id: 6,
+//       children: [
+//         {
+//           name: 'Calls',
+//           path: '/calls',
+//           icon: <Icons.PhoneForwardedRounded />,
+//           id: 6.1,
+//         },
 
-        {
-          name: 'Dial Plans',
-          path: '/dialplans',
-          icon: <Icons.CallSplitRounded />,
-          id: 6.2,
-        },
-        {
-          name: 'Numbers',
-          path: 'numbers',
-          icon: <Icons.DialpadRounded />,
-          id: 6.3,
-        },
-        {
-          name: 'Number Blocks',
-          path: '/numberblocks',
-          icon: <Icons.ViewModuleRounded />,
-          id: 6.4,
-        },
-        {
-          name: 'Trunks',
-          path: '/trunks',
-          icon: <Icons.NatRounded />,
-          id: 6.5,
-        },
-        {
-          name: 'Voice Policies',
-          path: '/voicepolicies',
-          icon: <Icons.PermDataSettingRounded />,
-          id: 6.6,
-        },
-      ],
-    },
-    {
-      name: 'GA',
-      icon: <Icons.AdminPanelSettings />,
-      id: 7,
-      children: [
-        {
-          name: 'Templates',
-          path: '/templates',
-          icon: <Icons.BorderClearRounded />,
-          id: 7.1,
-        },
-        {
-          name: 'Engines',
-          path: '/engines',
-          icon: <Icons.MiscellaneousServicesRounded />,
-          id: 7.2,
-        },
-        {
-          name: 'Wholesalers',
-          path: '/wholesalers',
-          icon: <Icons.LocationCityRounded />,
-          id: 7.3,
-        },
-        {
-          name: 'Carriers',
-          path: '/carriers',
-          icon: <Icons.MultipleStopRounded />,
-          id: 7.4,
-        },
-        {
-          name: 'Regions and Locations',
-          path: '/regions',
-          icon: <Icons.LocationOnRounded />,
-          id: 7.5,
-        },
-        {
-          name: 'Special Numbers',
-          path: '/special',
-          icon: <Icons.PasswordRounded />,
-          id: 7.6,
-        },
-      ],
-    },
-  ],
-};
+//         {
+//           name: 'Dial Plans',
+//           path: '/dialplans',
+//           icon: <Icons.CallSplitRounded />,
+//           id: 6.2,
+//         },
+//         {
+//           name: 'Numbers',
+//           path: 'numbers',
+//           icon: <Icons.DialpadRounded />,
+//           id: 6.3,
+//         },
+//         {
+//           name: 'Number Blocks',
+//           path: '/numberblocks',
+//           icon: <Icons.ViewModuleRounded />,
+//           id: 6.4,
+//         },
+//         {
+//           name: 'Trunks',
+//           path: '/trunks',
+//           icon: <Icons.NatRounded />,
+//           id: 6.5,
+//         },
+//         {
+//           name: 'Voice Policies',
+//           path: '/voicepolicies',
+//           icon: <Icons.PermDataSettingRounded />,
+//           id: 6.6,
+//         },
+//       ],
+//     },
+//     {
+//       name: 'GA',
+//       icon: <Icons.AdminPanelSettings />,
+//       id: 7,
+//       children: [
+//         {
+//           name: 'Templates',
+//           path: '/templates',
+//           icon: <Icons.BorderClearRounded />,
+//           id: 7.1,
+//         },
+//         {
+//           name: 'Engines',
+//           path: '/engines',
+//           icon: <Icons.MiscellaneousServicesRounded />,
+//           id: 7.2,
+//         },
+//         {
+//           name: 'Wholesalers',
+//           path: '/wholesalers',
+//           icon: <Icons.LocationCityRounded />,
+//           id: 7.3,
+//         },
+//         {
+//           name: 'Carriers',
+//           path: '/carriers',
+//           icon: <Icons.MultipleStopRounded />,
+//           id: 7.4,
+//         },
+//         {
+//           name: 'Regions and Locations',
+//           path: '/regions',
+//           icon: <Icons.LocationOnRounded />,
+//           id: 7.5,
+//         },
+//         {
+//           name: 'Special Numbers',
+//           path: '/special',
+//           icon: <Icons.PasswordRounded />,
+//           id: 7.6,
+//         },
+//       ],
+//     },
+//   ],
+// };
